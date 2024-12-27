e35bd8ffbf8f13266816af3468992544fa50b3f0
Sahil Warkhade <sahilwarkhade111@gmail.com>
2024-12-27 22:13:37 +0530

    Error Fixed

--- JS/emailSender.js
+++ JS/emailSender.js
@@ -4,21 +4,6 @@ let senderEmail=document.getElementById("senderEmail");
 let senderName=document.getElementById("senderName");
 let form=document.querySelector("form");
 
-// function sendEmail(){
-//     const bodyOFEmail=`FullName: ${senderName.value} <br> Email: ${senderEmail.value} <br> Message: ${senderMessage.value}`
-//     Email.send({
-//         Host : "smtp.elasticemail.com",
-//         Username : "sahilwarkhade111@gmail.com",
-//         Password : "A01E460E00C690E0977267EC3BB6AD167BF8",
-//         To : 'sahilwarkhade111@gmail.com',
-//         From : "sahilwarkhade111@gmail.com",
-//         Subject : senderSubject.value,
-//         Body : bodyOFEmail,
-//     }).then(
-//       message => alert("Email Send Successfully")
-//     ).catch(err=>alert("Email not send"))
-// }
-
 function sendEmail(){
     let params={
         name:senderName.value,
@@ -42,14 +27,3 @@ form.addEventListener("submit",(e)=>{
     senderName.value="";
     senderSubject.value="";
 })
-
-
-let linkedin=document.getElementById("linkedin-icon");
-
-console.log(linkedin);
-function redirect(url) {
-    window.location.href = url;
-    // window.open(url)
-    console.log("iam inside")
-    
-}
